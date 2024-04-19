5e5512693c8bdbac1de9f0907c7510b4560c2b05
yurakovbuz1 <yura.kovbuz1@gmail.com>
2024-04-19 19:39:38 +0200

    Form finished

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+console.log("HERE")
 const images = [
   {
     preview:
---
+++ src/js/2-form.js
@@ -0,0 +1,68 @@
+const KEY = "feedback-form-state";
+const form = document.querySelector(".feedback-form");
+const input = form.querySelector("input");
+const textarea = form.querySelector("textarea");
+// const button = form.querySelector("button");
+// button.addEventListener("click", onButtonClick)
+textarea.addEventListener("input", handleTextareaPress);
+input.addEventListener("input", handleInputPress)
+form.addEventListener("click", onFormSubmit)
+
+getStoredData();
+
+let info = {};
+
+function getStoredData() {
+    const savedMessage = JSON.parse(localStorage.getItem(KEY));
+    if (savedMessage) {
+        input.value = savedMessage.email;
+        textarea.textContent = savedMessage.message;
+    }
+}
+
+function handleInputPress(e) {
+    if (e.target.nodeName !== "INPUT") return; 
+    const email = e.target.value;
+    info.email = email;
+    localStorage.setItem(KEY, JSON.stringify(info));
+};
+
+function handleTextareaPress(e) {
+    if (e.target.nodeName !== "TEXTAREA") return; 
+    const message = e.target.value;
+    info.message = message;
+    localStorage.setItem(KEY, JSON.stringify(info));
+};
+
+function onFormSubmit(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== "BUTTON") return; 
+    e.preventDefault();
+    if (input.value === "") {
+        alert("Please, fill in your name.")
+    }
+    else if (textarea.value === "") {
+        alert("Don't forget to leave your message!")
+    } else {
+    console.log(JSON.parse(localStorage.getItem(KEY)))
+    textarea.value = "";
+    input.value = "";
+        localStorage.clear();
+    }
+}
+
+// function onButtonClick(e) {
+//     if (e.target.nodeName !== "BUTTON") return; 
+//     e.preventDefault();
+//     if (input.value === "") {
+//         alert("Please, fill in your name.")
+//     }
+//     else if (textarea.value === "") {
+//         alert("Don't forget to leave your message!")
+//     } else {
+//     console.log(JSON.parse(localStorage.getItem(KEY)))
+//     textarea.value = "";
+//     input.value = "";
+//         localStorage.clear();
+//     }
+// }
